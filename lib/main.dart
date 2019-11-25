682415b98ddf36644edcc52e7049bc484696e5a7
Brett Kolodny <brettkolodny@gmail.com>
2019-11-25 14:44:52 -0500

    Nutrition API call in popup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 List<CameraDescription> cameras;
 
@@ -208,14 +208,22 @@ class _SearchWidgetState extends State<SearchWidget> {
       floatingActionButton: FloatingActionButton(
         // When the user presses the button, show an alert dialog containing
         // the text that the user has entered into the text field.
-        onPressed: () {
+        onPressed: () async {
+          var url =
+              "https://trackapi.nutritionix.com/v2/search/instant?query=${searchController.text}&detailed=true";
+          var headers = {
+            "x-app-id": "314dccf6",
+            "x-app-key": "3ff9cb2d68cd00e7779deb4fa93fea07"
+          };
+          var response = await http.get(url, headers: headers);
+
           return showDialog(
             context: context,
             builder: (context) {
               return AlertDialog(
                 // Retrieve the text the that user has entered by using the
                 // TextEditingController.
-                content: Text(searchController.text),
+                content: Text(response.body),
               );
             },
           );
